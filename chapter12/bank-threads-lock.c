4b1813ea65d27cd375d031bd36e402e5325507d5
Nadeem Abdul Hamid <nadeem@acm.org>
2022-11-07 11:38:24 -0500

    lock example

--- chapter12/bank-threads-lock.c
+++ chapter12/bank-threads-lock.c
@@ -2,15 +2,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 void *depositor(void *arg);
 void *withdrawer(void *arg);
+
+pthread_mutex_t work_mutex;
+
 int balance = 0;
 
 int main() {
     int res;
     pthread_t T1, T2;
     void *thread_result;
+
+    if (pthread_mutex_init(&work_mutex, NULL) != 0) { perror("mutex init"); exit(EXIT_FAILURE); }
 
     res = pthread_create(&T1, NULL, depositor, NULL);    
     if (res != 0) {
@@ -40,12 +46,15 @@ int main() {
 
     printf("Final balance: %d\n", balance);
 
+    pthread_mutex_destroy(&work_mutex);
+
     exit(EXIT_SUCCESS);
 }
 
 void *depositor(void *arg) {
     int total = 0;
     for (int i = 0; i < 100; i++) {
+        pthread_mutex_lock(&work_mutex);
         usleep(random() % 3);
         /* also, try this instead of the next three lines: 
         balance = balance + 10;
@@ -53,6 +62,7 @@ void *depositor(void *arg) {
         balance = balance + 4;
         usleep(random() % 3);
         balance = balance + 6; 
+        pthread_mutex_unlock(&work_mutex);
         printf("Dep: %d\n", balance);
         total += 10;
     }
@@ -65,12 +75,17 @@ void *depositor(void *arg) {
 void *withdrawer(void *arg) {
     int total = 0;
     for (int i = 0; i < 100; i++) {
-        while (balance <= 5) ;   /* can't do anything here */
+        pthread_mutex_lock(&work_mutex);
         int x = balance;
-        usleep(random() % 5);
-        balance = x - 5;
-        printf("With: %d\n", balance);
-        total += 5;
+        if (balance > 5) {
+            usleep(random() % 5);
+            balance = x - 5;
+            total += 5;
+            printf("With: %d\n", balance);
+        } else {
+            i--;  /* force extra round */
+        }
+        pthread_mutex_unlock(&work_mutex);
     }
 
     printf("Withdrawn total: %d\n", total);
